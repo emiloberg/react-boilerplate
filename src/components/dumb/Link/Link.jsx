c9077c0d1aca89a0edbf8bce7e89dacfb049c377
Emil Öberg <emil.oberg@monator.com>
2015-12-19 17:04:53 +0100

    Example of hot load component.
    Reworked Link component to use ES6 style class instead of pure function, to show how hot loader works.
    
    Currently the hot loader isn't working with pure function components.

--- src/components/dumb/Link/Link.jsx
+++ src/components/dumb/Link/Link.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 
-const Link = ({ active, children, onClick }) => {
-	if (active) {
-		return (<span>{children}</span>);
-	}
-	return (
-		<a href="#" onClick={(e) => {
+class Link extends React.Component {
+	render() {
+		const { active, children, onClick } = this.props;
+		if (active) {
+			return (<span>{children}</span>);
+		}
+		return (
+			<a href="#" onClick={(e) => {
 			e.preventDefault();
 			onClick();
 		}}>
-			{children}
-		</a>
-	);
+				{children}
+			</a>
+		);
+	}
+}
+
+Link.propTypes = {
+	active: React.PropTypes.bool,
+	children: React.PropTypes.string,
+	onClick: React.PropTypes.func
 };
 
 export default Link;
